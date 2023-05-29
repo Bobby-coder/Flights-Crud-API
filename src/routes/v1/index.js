7c8930a01b806b701ceff9f3b5371fd825beec6a
Bobby-coder <bobbysadhwani612@gmail.com>
2023-05-29 12:08:02 +0530

    imports and exports  done

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const { infoController } = require("../../controllers");
 
+const airplaneRoutes = require("./airplane-route");
+
 const router = express.Router();
+
+router.use("./airplane", airplaneRoutes);
 
 router.get("/info", infoController.info);
 
