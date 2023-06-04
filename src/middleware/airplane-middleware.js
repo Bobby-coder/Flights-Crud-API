6abb921eb7dc8a31f621d9bc0a6fa70088a74e93
Bobby-coder <bobbysadhwani612@gmail.com>
2023-06-04 11:13:14 +0530

    Error handled in proper way

--- src/middleware/airplane-middleware.js
+++ src/middleware/airplane-middleware.js
@@ -2,12 +2,15 @@ const { StatusCodes } = require("http-status-codes");
 
 const { ErrorResponse } = require("../utils/common");
 
+const AppError = require("../utils/errors/app-error");
+
 function validateCreateRequest(req, res, next) {
   if (!req.body.modelNumber) {
     ErrorResponse.message = "Something went wrong while creating airplane";
-    ErrorResponse.error = {
-      explanation: "Model number does not found in the incoming request",
-    };
+    ErrorResponse.error = new AppError(
+      ["Model number does not found in the incoming request"],
+      StatusCodes.BAD_REQUEST
+    );
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   next();
